51ccacadee16918c1d0f2c2572cce383df4bb8d8
user-1 <user-1@MacBook-Pro.local>
2020-10-20 18:01:10 -0600

    basic structure of main page and some styling

--- src/App.js
+++ src/App.js
@@ -6,10 +6,31 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Morgan Shaw</h1>
+        <h1 className='typewriter'>Hi, I'm Morgan Shaw.</h1>
       </header>
-      <div className='bricks'>
-        <div>TEST</div>
+      <div className='picture-border'></div>
+      <div className='main-section'>
+        <div className='column'>
+          <div className='child left'>
+            {/* <div className='overlay'> */}
+              <span className='text'>ABOUT</span>
+            {/* </div> */}
+          </div>
+        </div>
+        <div className='column'>
+          <div className='child mid'>
+            {/* <div className='overlay'> */}
+              <span className='text'>WORK</span>
+            {/* </div> */}
+          </div>
+        </div>
+        <div className='column'>
+          <div className='child right'>
+            {/* <div className='overlay'> */}
+              <span className='text'>CONTACT</span>
+            {/* </div> */}
+          </div>
+        </div>
       </div>
     </div>
   );
