325189679d8c01b082d971a6ba2d8bfade4c3100
user-1 <user-1@MacBook-Pro.local>
2020-10-28 15:38:42 -0600

    started redesign in new branch

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 // import {Link} from 'react-router-dom';
-
+import logo from './Initials_Logo_MS.png';
+import me from './seated_in_chair_dog.jpeg';
 import './reset.css';
 import './App.css';
 // import './Components/About';
@@ -10,36 +11,28 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1 className='typewriter'>Hi, I'm Morgan Shaw.</h1>
-      </header>
-      <div className='picture-border'></div>
-      <div className='main-section'>
-        <div className='column'>
-          <div className='child-left'>
-            {/* <div className='overlay'> */}
-              <span className='text'>ABOUT</span>
-            {/* </div> */}
+      <section className='whole-page'>
+        <header className="App-header">
+          <div className='left-stuff'>
+            <div className='logo-container'>
+              <div className='logo'>MS</div>
+              {/* <img src={logo} alt='logo'/> */}
+            </div>
+            <div className='name-title'>
+              <h1 className='name'>Morgan Shaw</h1>
+              <h1 className='title'>|  Web Developer</h1>
+            </div>
           </div>
-        </div>
-        <div className='column'>
-          <div className='child-mid'>
-            {/* <div className='overlay'> */}
-              <span className='text'>WORK</span>
-              {/* <Link to='/work'> */}
-                <span className='small-text'>Take a tour <br/>of my work.</span>
-              {/* </Link>  */}
-            {/* </div> */}
+          <div className='links'>
+            <h2>Home</h2>
+            <h2>Projects</h2>
+            <h2>Contact</h2>
           </div>
+        </header>
+        <div className='picture'>
+          {/* <img src={me} alt='seated with laptop and dog'/> */}
         </div>
-        <div className='column'>
-          <div className='child-right'>
-            {/* <div className='overlay'> */}
-              <span className='text'>CONTACT</span>
-            {/* </div> */}
-          </div>
-        </div>
-      </div>
+      </section>
     </div>
   );
 }
